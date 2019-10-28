05edaa334db1089eab525cd48cdbe33802d7c15e
Nuruddin Ashr <uudashr@gmail.com>
2019-10-28 14:33:27 +0700

    Add index for category_id

---
+++ internal/mongo/migrations/4_add_index_on_products_cat.go
@@ -0,0 +1,24 @@
+package migration
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func init() {
+	migrate.MustRegister(func(db *mongo.Database) error {
+		opts := options.Index().SetName("ix_cat")
+		keys := bson.M{"categoryId": 1}
+		model := mongo.IndexModel{Keys: keys, Options: opts}
+
+		_, err := db.Collection("products").Indexes().CreateOne(context.TODO(), model)
+		return err
+	}, func(db *mongo.Database) error {
+		return db.Collection("products").Drop(context.TODO())
+	})
+}
